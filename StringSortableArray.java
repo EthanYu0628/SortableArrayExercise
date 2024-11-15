a7a1e61dd123a3c02b8c8ad8f19bd12413f429ab
EthanYu0628 <yuethan888@gmail.com>
2024-11-14 18:19:44 -0800

    added integer sorting methods

--- StringSortableArray.java
+++ StringSortableArray.java
@@ -30,4 +30,15 @@ public class StringSortableArray {
     public List<String> getList() {
         return strList;
     }
+
+    // Main method to test string sorting
+    public static void main(String[] args) {
+        StringSortableArray stringArray = new StringSortableArray("banana", "apple", "cherry", "date");
+
+        System.out.println("Original String List: " + stringArray.getList());
+        stringArray.sortAsc();
+        System.out.println("Ascending: " + stringArray.getList());
+        stringArray.sortDesc();
+        System.out.println("Descending: " + stringArray.getList());
+    }
 }
