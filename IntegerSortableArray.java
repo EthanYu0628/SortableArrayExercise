74954def1f3482dc166481f28016972b8bbe7e14
EthanYu0628 <yuethan888@gmail.com>
2024-11-14 18:55:27 -0800

    added integer sorting methods

--- IntegerSortableArray.java
+++ IntegerSortableArray.java
@@ -6,31 +6,27 @@ public class IntegerSortableArray {
 
     private List<Integer> intList;
 
-    // Constructor for integer list
     public IntegerSortableArray(List<Integer> intList) {
         this.intList = new ArrayList<>(intList);
     }
 
-    // Method to sort integers in ascending order
+
     public void sortAsc() {
         if (intList != null) {
             Collections.sort(intList);
         }
     }
 
-    // Method to sort integers in descending order
     public void sortDesc() {
         if (intList != null) {
             intList.sort(Collections.reverseOrder());
         }
     }
 
-    // Getter for integer list
     public List<Integer> getList() {
         return intList;
     }
 
-    // Main method to test integer sorting
     public static void main(String[] args) {
         List<Integer> integers = List.of(5, 3, 8, 1, 9);
         IntegerSortableArray integerArray = new IntegerSortableArray(integers);
